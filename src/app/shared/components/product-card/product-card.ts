a01c3c968e55d79c5175bbb56131596a747d92c4
Roman Nikolaenko <roma.nikolaenko.91@gmail.com>
2025-08-29 20:55:59 +0300

    edit

--- src/app/shared/components/product-card/product-card.ts
+++ src/app/shared/components/product-card/product-card.ts
@@ -2,7 +2,6 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 import { CardModel } from '^interfaces/card';
-
 import { Rating } from '../rating/rating';
 
 @Component({
